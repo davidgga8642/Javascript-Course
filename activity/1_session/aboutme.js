4d6137d606cae77014080ea0856f6fdefd2e4afa
davidgga8642 <102518652+davidgga8642@users.noreply.github.com>
2025-04-23 11:21:01 +0200

    Update aboutme.js

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,14 @@
-console.log("hello world");
+let NombreCreador;
+NombreCompleto="David Gómez García-Arias"
+console.log(NombreCompleto);
+
+let profesion="Tecnico de campo"
+console.log(profesion)
+
+let puestoDeseado;
+puestoDeseado="Programador"
+console.log(puestoDeseado);
 
 
-console.log("laater");
+const presentacion = `Hola, me llamo ${nombreCompleto} y me dedico a ${profesion}. Estoy cursando este Máster porque me gustaría trabajar en ${puestoDeseado}.`;
+console.log(presentacion);
