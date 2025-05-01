68747beb917d3d84e2d149b028d06754d6d3420f
davidgga8642 <102518652+davidgga8642@users.noreply.github.com>
2025-05-01 12:11:26 +0200

    Update index.js

--- activity/2_session/index.js
+++ activity/2_session/index.js
@@ -1,6 +1,46 @@
 // Ejercicio 1
-
+const getProperties = obj => Object.keys(obj);
+let person = {
+  name: "David",
+  age: 22,
+  profession: "tecnico campo",
+};
+console.log(getProperties(person)); 
 // Ejercicio 2
+// En el contexto global (modo no estricto):
+console.log(this); 
+//En una función (modo estricto):
+'use strict';
+function show() {
+  console.log(this); 
+}
+show();
+// Dentro de un objeto (método):
+const obj = {
+  name: "David",
+  sayHi() {
+    console.log(this.name); 
+  }
+};
+obj.sayHi();
+//En un constructor (new keyword):
+function Person(name) {
+  this.name = name;
+}
+const p = new Person("Jose");
+console.log(p.name); 
+//En un evento DOM (HTML/JS):
+<button onclick="console.log(this)">Click me</button> 
+//En arrow functions (hereda de contexto padre):
+const obj = {
+  name: "David",
+  greet: () => { 
+    console.log(this.name); 
+  }
+};
+obj.greet();
+//2.2 Enumera las diferencias entre arrow functions y function expressions.
+
 
 // Ejercicio 3
 
